037c7f52ebe8319784f3f1eb15c0ab5d8343d042
Jason <dimensions3@outlook.com>
2022-08-06 20:56:12 -0700

    getting compilation working

--- hvyc_eth/hvyc_eth/hvyc_eth.py
+++ hvyc_eth/hvyc_eth/hvyc_eth.py
@@ -1,12 +1,18 @@
-"""Heavy Meta HVYC Ethereum Contract Handler."""
+"""
+Heavy Meta HVYC & HVYI Ethereum Contract Handler.
+Created on Wed Aug  3 12:20:54 2022
+
+@author: meta-cronos
+"""
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 from jinja2 import Environment, FileSystemLoader
+import solcx
 import json
 import os
 
 SCRIPT_DIR = os.path.abspath( os.path.dirname( __file__ ) )
-BURNABLE_IMPORT = 'import "@openzeppelin/contracts@4.7.2/token/ERC721/extensions/ERC721Burnable.sol"'
+BURNABLE_IMPORT = 'import "./@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";'
 BURNABLE_TAG = ' ERC721Burnable, '
 UINT_256 = 'uint256 '
 UINT_MINT = 'uint256 {{intProp}} = (((randArr[0] % 100000000) / 1000000) % 18);'
@@ -14,6 +20,7 @@ UINT_MINT = 'uint256 {{intProp}} = (((randArr[0] % 100000000) / 1000000) % 18);'
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 contract_template = 'hvy_template.txt'
+sol_version = '0.8.4'
 
 #Begin Contract Data Classes-----------------------------
 
@@ -49,6 +56,8 @@ class hvym_data:
     :type abrev:  (str)
     :param name: String identifier defined by the creator.
     :type name:  (str)
+    :param payable: String identifier "payable"( minting anyone for a price), "onlyOwner(minted by owner for a price)"
+    :type payable:  (str)
     :param burnable: bool if burnable or not.
     :type burnable:  (bool)
     :param burnable_import: String script tag for burnable.sol contract.
@@ -77,6 +86,7 @@ class hvym_data:
     model: str
     abrev: str
     name: str
+    payable: str
     burnable: False
     burnable_import: str
     burnable_tag: str
@@ -91,6 +101,24 @@ class hvym_data:
     
 
 #Begin Contract Data Classes-----------------------------
+def SolInstall(version):
+    versions = solcx.get_installed_solc_versions()
+    
+
+    
+    if not version in versions:
+        solcx.install_solc(version)
+
+def Compile(contract):
+    compiled = solcx.compile_source(
+        contract,
+        output_values=["abi", "bin-runtime"],
+        solc_version=sol_version,
+        import_remappings=["@openzeppelin=C:/Users/pc/node_modules/@openzeppelin"]
+    )
+    
+    print(compiled)
+    
 
 def HVYM_DATA(name, price, prem_price, max_supply, prop_arr, meta_arr):
     '''
@@ -116,15 +144,16 @@ def HVYM_DATA(name, price, prem_price, max_supply, prop_arr, meta_arr):
     #parse the properties to the right format, for contract rendering.
     index = 0
     for prop in prop_arr:
-        obj.intProps.append('unint256 '+prop+';')
-        obj.intMintProps.append('unint256 '+prop+'  = (((randArr[0] % 100000000) / 1000000) % 18);')
+        print(prop)
+        obj.intProps.append(prop+';')
+        obj.intMintProps.append(prop+'  = (((randArr[0] % 100000000) / 1000000) % 18);')
         
         if index < (len(prop_arr)-1):
-            obj.intParams += ' unint256 '+prop+','
+            obj.intParams += ' uint256 '+prop+','
         else:
-            obj.intParams += ' unint256 '+prop
+            obj.intParams += ' uint256 '+prop
             
-        obj.intPush.append('unint256 '+prop)
+        obj.intPush.append(prop)
         
         index += 1
         
@@ -132,13 +161,13 @@ def HVYM_DATA(name, price, prem_price, max_supply, prop_arr, meta_arr):
     return obj
 
 
-
-def HVYCharacter(name, price, prem_price, max_supply, prop_arr, meta_arr):
+def HVYCharacter_Payable(name, price, prem_price, max_supply, prop_arr, meta_arr):
     '''
-    Creates a burnable HVYC ERC721 (C: Character / burnable) contract from 
-    template based on passed parameters. Used for a character that has a
-    limited life.  So that it may be burned, when conditions of the game
-    would cause it to expire / die / be killed.
+    Creates a payable, burnable HVYC ERC721 (C: Character / burnable) contract from 
+    template based on passed parameters. Payable means: any address can pay to mint
+    the NFT. Used for a character that has a limited life.  So that it may be burned, 
+    when conditions of the game would cause it to expire / die / be killed.
+    
     :param name: string of the minter contract name
     :type name:  (str)
     :param price:  Price for NFT with randomly generated number properties.
@@ -162,16 +191,55 @@ def HVYCharacter(name, price, prem_price, max_supply, prop_arr, meta_arr):
     intParams = obj.intParams
     intPush = obj.intPush
         
-    data = hvym_data('Character', 'C', name, True, BURNABLE_IMPORT, BURNABLE_TAG, price, prem_price, max_supply, intProps, intMintProps, intParams, intPush, prop_arr )
+    data = hvym_data('Character', 'C', name, 'payable', True, BURNABLE_IMPORT, BURNABLE_TAG, price, prem_price, max_supply, intProps, intMintProps, intParams, intPush, prop_arr )
     
     output = template.render(data=data)
     print(output)
+    Compile(output)
     
-def HVYImmortal(name, price, prem_price, max_supply, prop_arr, meta_arr):
+def HVYCharacter_OnlyOwner(name, price, prem_price, max_supply, prop_arr, meta_arr):
     '''
-    Creates a burnable HVYI (I: Immortal / not-burnable) ERC721 contract 
-    from template based on passed. Used for a character that has an infinite
-    life.  Cannot be burnt by any condition of the game.
+    Creates an owner only, burnable HVYC ERC721 (C: Character / burnable) contract from 
+    template based on passed parameters. Owner only means only the issuing address can
+    mint the NFT. Used for a character that has a limited life.  So that it may be 
+    burned, when conditions of the game would cause it to expire / die / be killed.
+    
+    :param name: string of the minter contract name
+    :type name:  (str)
+    :param price:  Price for NFT with randomly generated number properties.
+    :type price: (str)
+    :param prem_price:  Price for NFT that user can set number properties.
+    :type prem_price: (str)
+    :param max_supply:  Maximum number of NFTs that can ever be minted by contract.
+    :type max_supply: (str)
+    :param prop_arr:  Array of number settable properties for the NFT.
+    :type prop_arr: (list[str])
+    :param meta_arr:  Array of static meta-data, based on Open Sea format.
+    :type meta_arr: (list[str])
+    :return: (bool) True if contract is created
+    :rtype: (bool)
+    '''
+    template = env.get_template(contract_template)
+    obj = HVYM_DATA(name, price, prem_price, max_supply, prop_arr, meta_arr)
+    
+    intProps = obj.intProps
+    intMintProps = obj.intMintProps
+    intParams = obj.intParams
+    intPush = obj.intPush
+        
+    data = hvym_data('Character', 'C', name, 'ownerOnly', True, BURNABLE_IMPORT, BURNABLE_TAG, price, prem_price, max_supply, intProps, intMintProps, intParams, intPush, prop_arr )
+    
+    output = template.render(data=data)
+    print(output)
+    Compile(output)
+    
+def HVYImmortal_Payable(name, price, prem_price, max_supply, prop_arr, meta_arr):
+    '''
+    Creates a payable, burnable HVYI (I: Immortal / not-burnable) ERC721 contract 
+    from template based on passed. Payable means: any address can pay to mint
+    the NFT. Used for a character that has an infinite life.  Cannot be burnt by 
+    any condition of the game.
+    
     parameters.
     :param name: string of the minter contract name
     :type name:  (str)
@@ -196,12 +264,52 @@ def HVYImmortal(name, price, prem_price, max_supply, prop_arr, meta_arr):
     intParams = obj.intParams
     intPush = obj.intPush
         
-    data = hvym_data('Immortal', 'I', name, False, BURNABLE_IMPORT, BURNABLE_TAG, price, prem_price, max_supply, intProps, intMintProps, intParams, intPush, prop_arr )
+    data = hvym_data('Immortal', 'I', name, 'payable', False, BURNABLE_IMPORT, BURNABLE_TAG, price, prem_price, max_supply, intProps, intMintProps, intParams, intPush, prop_arr )
     
     output = template.render(data=data)
     print(output)
+    Compile(output)
+    
+def HVYImmortal_OnlyOwner(name, price, prem_price, max_supply, prop_arr, meta_arr):
+    '''
+    Creates a burnable HVYI (I: Immortal / not-burnable) ERC721 contract 
+    from template based on passed. Owner only means only the issuing address can
+    mint the NFT. Used for a character that has an infinite life.  Cannot be 
+    burnt by any condition of the game.
+    
+    parameters.
+    :param name: string of the minter contract name
+    :type name:  (str)
+    :param price:  Price for NFT with randomly generated number properties.
+    :type price: (str)
+    :param prem_price:  Price for NFT that user can set number properties.
+    :type prem_price: (str)
+    :param max_supply:  Maximum number of NFTs that can ever be minted by contract.
+    :type max_supply: (str)
+    :param prop_arr:  Array of number settable properties for the NFT.
+    :type prop_arr: (list[str])
+    :param meta_arr:  Array of static meta-data, based on Open Sea format.
+    :type meta_arr: (list[str])
+    :return: (bool) True if contract is created
+    :rtype: (bool)
+    '''
+    template = env.get_template(contract_template)
+    obj = HVYM_DATA(name, price, prem_price, max_supply, prop_arr, meta_arr)
+    
+    intProps = obj.intProps
+    intMintProps = obj.intMintProps
+    intParams = obj.intParams
+    intPush = obj.intPush
+        
+    data = hvym_data('Immortal', 'I', name, 'onlyOwner', False, BURNABLE_IMPORT, BURNABLE_TAG, price, prem_price, max_supply, intProps, intMintProps, intParams, intPush, prop_arr )
+    
+    output = template.render(data=data)
+    print(output)
+    Compile(output)
         
 
 props = ['intelligence', 'strength', 'hate', 'love', 'charm']
 
-HVYImmortal('TEST', '0.2', '0.25', '10', props, None)
+SolInstall(sol_version)
+
+HVYCharacter_Payable('TEST', '0.2', '0.25', '10', props, None)
